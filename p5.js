8cecd0eaa2e56674550636b03d59b13c97f92adf
novb1492 <novb1492@naver.com>
2021-05-04 23:34:33 +0900

    언덕그리기 설명할차례

---
+++ p5.js
@@ -0,0 +1,39 @@
+var spritedata;
+var spritesheet;
+let animation=[];
+let sheep;
+let hill;
+let point;
+function preload()
+{ 
+  //call sheep image and position
+  spritedata= loadJSON('p5/horse/horse.json');
+  spritesheet = loadImage('p5/sheep.png');
+}
+
+function setup() {
+  var framerate=60;
+  frameRate(framerate);
+    createCanvas(displayWidth,displayHeight);
+    var frames=[],animation=[];
+    frames=spritedata.frames;
+    for (var i = 0; i < frames.length; i++) {
+      var pos = frames[i].position;
+      var img = spritesheet.get(pos.x, pos.y, pos.w, pos.h);
+      animation.push(img);
+    }
+    var about=[startframe=0,framspeed=10];
+    sheep=new sprite(animation,about);
+    hill=new Bezier(480);
+}
+var x=500,y=500,i=0;
+var catcharray=[];
+  function draw() 
+  {  
+   background('gray');
+   hill.drawbezier();
+   i++;
+   catcharray=hill.getXY(i);
+    sheep.draw(catcharray);
+  }
+ 
